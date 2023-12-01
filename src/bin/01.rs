a0279e76fe572e5714d3d2f3e1c5796c3f83fc62
Alex Lepsa <lepsalex@gmail.com>
2023-12-01 13:45:20 -0500

    day 1 - part 2 (solved)

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,7 +1,7 @@
+use std::collections::HashMap;
 advent_of_code::solution!(1);
 
 pub fn part_one(input: &str) -> Option<u32> {
-
     let mut total = 0;
 
     let mut first: Option<char> = None;
@@ -35,7 +35,48 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let digit_words = vec!["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+    let mut total = 0;
+
+    for line in input.split(0xA as char) {
+        let mut digit_map: HashMap<usize, char> = HashMap::new();
+
+        // find number digits and store their index to the hash map
+        // as the key, with the digit char as the value
+        for (idx, char) in line.chars().enumerate() {
+            if char.is_digit(10) {
+                digit_map.insert(idx, char);
+            }
+        }
+
+        // find word digits and store their index to the hash map,
+        // for the value store the digit char instead of the word
+        for (dword_idx, dword) in digit_words.iter().enumerate() {
+            for (match_idx, _) in line.match_indices(dword) {
+                digit_map.insert(match_idx, char::from_digit((dword_idx + 1) as u32, 10).unwrap());
+            }
+        }
+
+        // if there were no digits continue
+        if digit_map.is_empty() {
+            continue
+        }
+
+        // Get the lowest and highest index chars
+        let min = digit_map.keys().min().unwrap();
+        let max = digit_map.keys().max().unwrap();
+
+        // put them together
+        let mut line_total_str = String::new();
+        line_total_str.push(digit_map.get(min).unwrap().clone());
+        line_total_str.push(digit_map.get(max).unwrap().clone());
+
+        // update total
+        total += line_total_str.parse::<u32>().unwrap();
+    }
+
+    Some(total)
 }
 
 #[cfg(test)]
@@ -45,12 +86,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, Some(142));
+        assert_eq!(result, Some(55090));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(292));
     }
 }
